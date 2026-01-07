06c3f3e15e1f4d899d8833ab72b45f406b759b5a
GugiChu <gauravchhajer613nkh@gmail.com>
2026-01-07 22:53:18 +0530

    08-functions

---
+++ 08-functions/src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const App = () => {
+  const mouseFollower = (e) => {
+    const pointer = document.querySelector(".pointer");
+    pointer.style.left = e.pageX + "px";
+    pointer.style.top = e.pageY + "px";
+  };
+
+  return (
+    <main onMouseMove={mouseFollower}>
+      <video
+        id="myVideo"
+        autoPlay
+        muted
+        loop
+        playsInline
+      >
+        <source src="/bg-effect.mp4" type="video/mp4" />
+      </video>
+
+      <div className="pointer"></div>
+    </main>
+  );
+};
+
+export default App;
